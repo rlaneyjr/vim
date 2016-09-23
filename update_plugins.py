68d3e0f8b55916c1fd308debd2b518e88e7b7086
Ricky Laney <rlaneyjr@gmail.com>
2016-09-23 13:36:15 -0400

    Auto Push

--- update_plugins.py
+++ update_plugins.py
@@ -15,23 +15,16 @@ bufexplorer https://github.com/corntrace/bufexplorer
 ctrlp.vim https://github.com/kien/ctrlp.vim
 dash.vim https://github.com/rizzatti/dash.vim
 goyo.vim https://github.com/junegunn/goyo.vim
-gruvbox https://github.com/morhetz/gruvbox
-mayansmoke https://github.com/vim-scripts/mayansmoke
 nerdtree https://github.com/scrooloose/nerdtree
 nginx.vim https://github.com/vim-scripts/nginx.vim
 open_file_under_cursor.vim https://github.com/amix/open_file_under_cursor.vim
-peaksea https://github.com/vim-scripts/peaksea
 python-mode https://github.com/klen/python-mode
 rbenv-aliases https://github.com/tpope/rbenv-aliases
 rbenv-ctags https://github.com/tpope/rbenv-ctags
 ruby-icloud https://github.com/adammck/ruby-icloud
-snipmate-snippets https://github.com/scrooloose/snipmate-snippets
-syntastic https://github.com/scrooloose/syntastic
 tComment https://github.com/vim-scripts/tComment
 tlib https://github.com/vim-scripts/tlib
 vim-addon-mw-utils https://github.com/MarcWeber/vim-addon-mw-utils
-vim-airline https://github.com/vim-airline/vim-airline
-vim-airline-themes https://github.com/vim-airline/vim-airline-themes
 vim-bundle-mako https://github.com/sophacles/vim-bundle-mako
 vim-bundler https://github.com/tpope/vim-bundler
 vim-coffee-script https://github.com/kchmck/vim-coffee-script
@@ -59,7 +52,6 @@ vim-repeat https://github.com/tpope/vim-repeat
 vim-rsi https://github.com/tpope/vim-rsi
 vim-ruby https://github.com/vim-ruby/vim-ruby
 vim-rvm https://github.com/tpope/vim-rvm
-vim-sensible https://github.com/tpope/vim-sensible
 vim-snipmate https://github.com/garbas/vim-snipmate
 vim-snippets https://github.com/honza/vim-snippets
 vim-surround https://github.com/tpope/vim-surround
@@ -69,7 +61,19 @@ vim-virtualenv https://github.com/jmcantrell/vim-virtualenv
 vim-zenroom2 https://github.com/amix/vim-zenroom2
 vimwiki https://github.com/vim-scripts/vimwiki
 seti.vim https://github.com/trusktr/seti.vim
+vim-easy-align https://github.com/junegunn/vim-easy-align
+molokai https://github.com/tomasr/molokai
 """.strip()
+
+# Removed plugins
+#vim-airline https://github.com/vim-airline/vim-airline
+#vim-airline-themes https://github.com/vim-airline/vim-airline-themes
+#gruvbox https://github.com/morhetz/gruvbox
+#mayansmoke https://github.com/vim-scripts/mayansmoke
+#peaksea https://github.com/vim-scripts/peaksea
+#snipmate-snippets https://github.com/scrooloose/snipmate-snippets
+#syntastic https://github.com/scrooloose/syntastic
+#vim-sensible https://github.com/tpope/vim-sensible
 
 GITHUB_ZIP = '%s/archive/master.zip'
 
