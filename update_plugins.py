5246b66650944a514a3bfffa3ff31e20819be997
Ricky Laney <rlaneyjr@gmail.com>
2017-02-27 10:26:58 -0500

    The git-update script pushed these

--- update_plugins.py
+++ update_plugins.py
@@ -8,6 +8,7 @@ from os import path
 
 #--- Globals ----------------------------------------------
 PLUGINS = """
+FastFold https://github.com/Konfekt/FastFold
 ack.vim https://github.com/mileszs/ack.vim
 ag.vim https://github.com/rking/ag.vim
 base16-vim https://github.com/chriskempson/base16-vim
@@ -15,6 +16,7 @@ bufexplorer https://github.com/corntrace/bufexplorer
 ctrlp.vim https://github.com/kien/ctrlp.vim
 dash.vim https://github.com/rizzatti/dash.vim
 goyo.vim https://github.com/junegunn/goyo.vim
+molokai https://github.com/tomasr/molokai
 nerdtree https://github.com/scrooloose/nerdtree
 nginx.vim https://github.com/vim-scripts/nginx.vim
 open_file_under_cursor.vim https://github.com/amix/open_file_under_cursor.vim
@@ -22,15 +24,19 @@ python-mode https://github.com/klen/python-mode
 rbenv-aliases https://github.com/tpope/rbenv-aliases
 rbenv-ctags https://github.com/tpope/rbenv-ctags
 ruby-icloud https://github.com/adammck/ruby-icloud
+seti.vim https://github.com/trusktr/seti.vim
 tComment https://github.com/vim-scripts/tComment
 tlib https://github.com/vim-scripts/tlib
 vim-addon-mw-utils https://github.com/MarcWeber/vim-addon-mw-utils
+vim-airline https://github.com/vim-airline/vim-airline
+vim-airline-themes https://github.com/vim-airline/vim-airline-themes
 vim-bundle-mako https://github.com/sophacles/vim-bundle-mako
 vim-bundler https://github.com/tpope/vim-bundler
 vim-coffee-script https://github.com/kchmck/vim-coffee-script
 vim-colors-solarized https://github.com/altercation/vim-colors-solarized
 vim-commentary https://github.com/tpope/vim-commentary
 vim-dotenv https://github.com/tpope/vim-dotenv
+vim-easy-align https://github.com/junegunn/vim-easy-align
 vim-endwise https://github.com/tpope/vim-endwise
 vim-eunuch https://github.com/tpope/vim-eunuch
 vim-expand-region https://github.com/terryma/vim-expand-region
@@ -42,9 +48,7 @@ vim-irblack https://github.com/wesgibbs/vim-irblack
 vim-jdaddy https://github.com/tpope/vim-jdaddy
 vim-less https://github.com/groenewege/vim-less
 vim-markdown https://github.com/tpope/vim-markdown
-vim-mkdir https://github.com/pbrisbin/vim-mkdir
 vim-multiple-cursors https://github.com/terryma/vim-multiple-cursors
-vim-pyte https://github.com/therubymug/vim-pyte
 vim-rails https://github.com/tpope/vim-rails
 vim-rake https://github.com/tpope/vim-rake
 vim-rbenv https://github.com/tpope/vim-rbenv
@@ -52,7 +56,6 @@ vim-repeat https://github.com/tpope/vim-repeat
 vim-rsi https://github.com/tpope/vim-rsi
 vim-ruby https://github.com/vim-ruby/vim-ruby
 vim-rvm https://github.com/tpope/vim-rvm
-vim-snipmate https://github.com/garbas/vim-snipmate
 vim-snippets https://github.com/honza/vim-snippets
 vim-surround https://github.com/tpope/vim-surround
 vim-tbone https://github.com/tpope/vim-tbone
@@ -60,20 +63,23 @@ vim-tmux-navigator https://github.com/christoomey/vim-tmux-navigator
 vim-virtualenv https://github.com/jmcantrell/vim-virtualenv
 vim-zenroom2 https://github.com/amix/vim-zenroom2
 vimwiki https://github.com/vim-scripts/vimwiki
-seti.vim https://github.com/trusktr/seti.vim
-vim-easy-align https://github.com/junegunn/vim-easy-align
-molokai https://github.com/tomasr/molokai
 """.strip()
 
 # Removed plugins
-#vim-airline https://github.com/vim-airline/vim-airline
-#vim-airline-themes https://github.com/vim-airline/vim-airline-themes
+'''
 #gruvbox https://github.com/morhetz/gruvbox
 #mayansmoke https://github.com/vim-scripts/mayansmoke
 #peaksea https://github.com/vim-scripts/peaksea
 #snipmate-snippets https://github.com/scrooloose/snipmate-snippets
+#snipmate-snippets https://github.com/scrooloose/snipmate-snippets
 #syntastic https://github.com/scrooloose/syntastic
+#syntastic https://github.com/scrooloose/syntastic
+#vim-mkdir https://github.com/pbrisbin/vim-mkdir
+#vim-pyte https://github.com/therubymug/vim-pyte
 #vim-sensible https://github.com/tpope/vim-sensible
+#vim-sensible https://github.com/tpope/vim-sensible
+#vim-snipmate https://github.com/garbas/vim-snipmate
+'''
 
 GITHUB_ZIP = '%s/archive/master.zip'
 
